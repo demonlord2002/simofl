4f9c156ca4ddd118ff63536cd104033b34a0ac9a
demonlord2002 <drdoom2003p@gmail.com>
2025-08-22 23:14:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_IDS = {int(uid.strip()) for uid in os.environ.get("OWNER_IDS", "").split(",") if uid.strip().isdigit()}
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7881851045:AAG8pEtMgAxwEtTQDfAODKLHVUB05ZpWTrY")
+OWNER_IDS = {int(uid.strip()) for uid in os.environ.get("OWNER_IDS", "7806800300").split(",") if uid.strip().isdigit()}
 AUTO_DELETE_SECONDS = int(os.getenv("AUTO_DELETE_SECONDS", "600"))  # default 10 min
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = os.environ.get("DB_NAME", "coolie_bot")
