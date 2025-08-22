77e4a87d969b559fcc5e19a1c3dfd646eeb382ef
demonlord2002 <drdoom2003p@gmail.com>
2025-08-22 23:18:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7881851045:AAG8pEtMgAxwEtTQDfAODKLHVUB05ZpWTrY")
 OWNER_IDS = {int(uid.strip()) for uid in os.environ.get("OWNER_IDS", "7806800300").split(",") if uid.strip().isdigit()}
 AUTO_DELETE_SECONDS = int(os.getenv("AUTO_DELETE_SECONDS", "600"))  # default 10 min
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "coolie_bot")
 COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "keywords")
