95863cfb77ec103c706581b70bbe6cc203a98828
demonlord2002 <drdoom2003p@gmail.com>
2025-08-23 10:35:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8246993253:AAEeu-qk1r1ajtq0YVF53YMxTKtz3aN7ae8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8373123085:AAHOk5tZb7gCjsgq5RK6JrsRJCfre1nZ1Zo")
 OWNER_IDS = {int(uid.strip()) for uid in os.environ.get("OWNER_IDS", "7806800300").split(",") if uid.strip().isdigit()}
 AUTO_DELETE_SECONDS = int(os.getenv("AUTO_DELETE_SECONDS", "600"))  # default 10 min
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority")
